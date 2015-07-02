15d1dbb59ec1d55919366d665c6069f738eb83d1
Francesc <f@gaxan.com>
2015-07-02 21:04:08 +0100

    Track last hit

--- battleship.py
+++ battleship.py
@@ -6,12 +6,17 @@ VALID = {'h', 'm', 's'}
 def get_random_pos():
 	return random.choice("ABCDEFGHIJ")+str(random.randint(1,10))
 
+def get_position_from_grid(grid, last_hit):
+	if last_hit is None:
+		return get_random_pos()
+	return get_random_pos()
 
 def main():
 	grid = {}
 	sunk = 0
+	last_hit = None
 	while sunk < 10:
-		pos = get_random_pos()
+		pos = get_position_from_grid(grid, last_hit)
 		if pos in grid:
 			continue
 		print(pos)
@@ -20,6 +25,9 @@ def main():
 			answer, VALID)
 		if answer == 's':
 			sunk += 1
+		if answer == 'h':
+			last_hit = pos
+
 		grid[pos] = answer
 
 
